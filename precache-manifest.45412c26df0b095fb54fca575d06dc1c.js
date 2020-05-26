c6d22d1f8ffa7b6db6d866eac8529afae354bfa2
unknown <dnicerio@gmail.com>
2020-05-26 22:12:38 +0800

    Updates

---
+++ precache-manifest.45412c26df0b095fb54fca575d06dc1c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b632f1bef31fc6818bbf765e7459d38c",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "e4e5603fc73e6ada76d9",
+    "url": "/random-quote-machine/static/css/2.3a81a6f2.chunk.css"
+  },
+  {
+    "revision": "d97eb6236c54082c3c39",
+    "url": "/random-quote-machine/static/css/main.52d6ef6a.chunk.css"
+  },
+  {
+    "revision": "e4e5603fc73e6ada76d9",
+    "url": "/random-quote-machine/static/js/2.859ad420.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.859ad420.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d97eb6236c54082c3c39",
+    "url": "/random-quote-machine/static/js/main.f9979487.chunk.js"
+  },
+  {
+    "revision": "169f538802cee213882f",
+    "url": "/random-quote-machine/static/js/runtime-main.4d3b854a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/random-quote-machine/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/random-quote-machine/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/random-quote-machine/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/random-quote-machine/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/random-quote-machine/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
